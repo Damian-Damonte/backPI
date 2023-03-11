508d0b345e061e9dd90053b314103b202d54e230
damiandamonte <damontedamian@gmail.com>
2023-03-11 11:58:22 -0300

    created JwtAuthenticationFilter

--- src/main/java/com/dh/digitalbooking/security/JwtService.java
+++ src/main/java/com/dh/digitalbooking/security/JwtService.java
@@ -6,12 +6,15 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Service
 public class JwtService {
     private static final String SECRET_KEY = "357638792F423F4428472B4B6250655368566D597133743677397A2443264629";
     public String extractUserneme(String token) {
